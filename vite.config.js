90a363673049e38d5e92245afae42ca8c0022902
Clemens <clemens.losbichler@gmail.com>
2025-01-06 13:13:11 +0100

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: import.meta.PROD ? "/graphs/" : "/",
+  base: "/graphs/",
   plugins: [
     vue(),
     vueJsx(),
